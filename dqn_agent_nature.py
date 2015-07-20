4191ae15cfaa057e4287eb5ec69e4a61c096c89d
naoto yoshida <ugo-nama-kun@users.noreply.github.com>
2015-07-21 02:20:39 +0900

    Update dqn_agent_nature.py

--- dqn_agent_nature.py
+++ dqn_agent_nature.py
@@ -21,7 +21,7 @@ from rlglue.types import Action
 class DQN_class:
     # Hyper-Parameters
     gamma = 0.99  # Discount factor
-    initial_exploration = 100#10**4  # Initial exploratoin. original: 5x10^4
+    initial_exploration = 10**4  # Initial exploratoin. original: 5x10^4
     replay_size = 32  # Replay (batch) size
     target_model_update_freq = 10**4  # Target update frequancy. original: 10^4
     data_size = 10**5  # Data size of history. original: 10^6
